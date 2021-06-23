33823c2c3a72f2c50a07d49a316f635ce15062bc
Andrii Kovalov <jfkwww1@gmail.com>
2021-06-23 13:59:30 +0000

    task_6 v.0.2 create_tickets.py fix for 000 001 to 999 999 tickets range

--- create_tickets_file.py
+++ create_tickets_file.py
@@ -1,4 +1,6 @@
 # Creating tickets.txt
 with open('tickets.txt', 'w') as f:
-    for i in range(100000, 1000000):
+    for i in range(1, 1000000):
+        i = i / 100000
+        i = f"{i:.5f}".replace('.', '')
         f.write(f'{i}\n')
