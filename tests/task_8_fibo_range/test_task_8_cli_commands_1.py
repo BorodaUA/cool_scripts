b993cd7c1f4cfa1f9f88ae107f049999c06d364c
Andrii Kovalov <jfkwww1@gmail.com>
2021-07-01 15:42:15 +0000

    task_8 tests v.0.2 additional tests

--- tests/task_8_fibo_range/test_task_8_cli_commands_1.py
+++ tests/task_8_fibo_range/test_task_8_cli_commands_1.py
@@ -1,3 +1,6 @@
+import random
+
+
 def test_fibo_range_cli_valid_parameters(cli_client):
     '''
     Test task_8_fibo_range/task_8.py valid parameters
@@ -69,5 +72,59 @@ def test_fibo_range_cli_start_range_bigger_end_range(cli_client):
         '"end_range" must be greater then "start_range"\n'
     )
     assert 1 == exitcode
+    assert error_msg == cli_error
+    assert b'' == cli_out
+
+
+def test_fibo_range_cli_start_range_not_int(cli_client):
+    '''
+    Test task_8_fibo_range/task_8.py start_range not int
+    '''
+    not_int_list = [{}, [], (), 'abc', None, True, b'def']
+    start_range = random.choice(not_int_list)
+    end_range = 200
+    command = (
+        'python',
+        'task_8_fibo_range/task_8.py',
+        '-sr',
+        f'{start_range}',
+        '-er',
+        f'{end_range}'
+    )
+    cli_out, cli_error, exitcode = cli_client(command)
+    cli_error = cli_error.decode()
+    #
+    error_msg = (
+        'usage: task_8.py [-h] [-sr START_RANGE] [-er END_RANGE]\ntask_8.py: '
+        f'error: argument -sr/--start_range: {start_range} is not an integer\n'
+    )
+    assert 2 == exitcode
+    assert error_msg == cli_error
+    assert b'' == cli_out
+
+
+def test_fibo_range_cli_end_range_not_positive_int(cli_client):
+    '''
+    Test task_8_fibo_range/task_8.py end_range not positive int
+    '''
+    start_range = 50
+    end_range = random.randint(-1234, -1)
+    command = (
+        'python',
+        'task_8_fibo_range/task_8.py',
+        '-sr',
+        f'{start_range}',
+        '-er',
+        f'{end_range}'
+    )
+    cli_out, cli_error, exitcode = cli_client(command)
+    cli_error = cli_error.decode()
+    #
+    error_msg = (
+        'usage: task_8.py [-h] [-sr START_RANGE] [-er END_RANGE]\ntask_8.py: '
+        'error: argument -er/--end_range: '
+        f'{end_range} is not a positive integer\n'
+    )
+    assert 2 == exitcode
     assert error_msg == cli_error
     assert b'' == cli_out
