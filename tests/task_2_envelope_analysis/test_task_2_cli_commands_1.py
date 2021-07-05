600a4421a125d44e296ab32a9347e84f27e2b218
Andrii Kovalov <jfkwww1@gmail.com>
2021-07-05 08:30:45 +0000

    task_2 tests v.0.2 additional tests

--- tests/task_2_envelope_analysis/test_task_2_cli_commands_1.py
+++ tests/task_2_envelope_analysis/test_task_2_cli_commands_1.py
@@ -146,3 +146,97 @@ def test_envelopes_cannot_fit_cli_valid_parameters(monkeypatch, capsys):
         )
     assert '' == cli_error
     assert expected_result == cli_out
+
+
+def test_quit_in_start_over_handler_cli(monkeypatch, capsys):
+    '''
+    Test "q" quit in start_over handler
+    envelopes with valid parameters
+    '''
+    envelope_1_side_a = str(float(random.randint(20, 30)))
+    envelope_1_side_b = str(float(random.randint(20, 30)))
+    #
+    envelope_2_side_a = str(float(random.randint(1, 19)))
+    envelope_2_side_b = str(float(random.randint(1, 19)))
+    start_over = 'q'
+    #
+    input_values = '\n'.join(
+        [
+            envelope_1_side_a,
+            envelope_1_side_b,
+            envelope_2_side_a,
+            envelope_2_side_b,
+            start_over
+        ]
+    )
+    #
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(input_values))
+    #
+    with pytest.raises(SystemExit):
+        env_analyzer = EnvelopesAnalyzer()
+        env_analyzer.main()
+    #
+    cli_out, cli_error = capsys.readouterr()
+    #
+    expected_result = (
+            '\n*** Welcome to the Envelope Analyzer *** \n'
+            'Follow the program instruction to find out which '
+            'Envelope can fit inside the other \n'
+            'You can quit at any moment by typing command: q\n'
+            '\nPlease enter Envelope 1 side A: '
+            'Please enter Envelope 1 side B: '
+            'Please enter Envelope 2 side C: '
+            'Please enter Envelope 2 side D: \n'
+            'Envelope 1 with sides: '
+            f'{envelope_1_side_a} and {envelope_1_side_b}\n'
+            'Envelope 2 with sides:  '
+            f'{envelope_2_side_a} and {envelope_2_side_b}\n'
+            'Envelope 2 can fit inside Envelope 1\n'
+            '\nWould you like to start over? \nType: y/yes or n/no '
+        )
+    assert '' == cli_error
+    assert expected_result == cli_out
+
+
+def test_quit_in_input_envelope_handler_cli(monkeypatch, capsys):
+    '''
+    Test "q" quit in input envelope handler
+    envelopes with valid parameters
+    '''
+    envelope_1_side_a = str(float(random.randint(20, 30)))
+    envelope_1_side_b = str(float(random.randint(20, 30)))
+    #
+    envelope_2_side_a = str(float(random.randint(1, 19)))
+    envelope_2_side_b = str(float(random.randint(1, 19)))
+    start_over = 'n'
+    quit_command = 'q'
+    #
+    input_values = '\n'.join(
+        [
+            envelope_1_side_a,
+            quit_command,
+            envelope_1_side_b,
+            envelope_2_side_a,
+            envelope_2_side_b,
+            start_over
+        ]
+    )
+    #
+    monkeypatch.setattr(sys, 'stdin', io.StringIO(input_values))
+    #
+    with pytest.raises(SystemExit):
+        env_analyzer = EnvelopesAnalyzer()
+        env_analyzer.main()
+    #
+    cli_out, cli_error = capsys.readouterr()
+    #
+    expected_result = (
+            '\n*** Welcome to the Envelope Analyzer *** \n'
+            'Follow the program instruction to find out which '
+            'Envelope can fit inside the other \n'
+            'You can quit at any moment by typing command: q\n'
+            '\nPlease enter Envelope 1 side A: '
+            'Please enter Envelope 1 side B: '
+        )
+    assert '' == cli_error
+    assert expected_result == cli_out
